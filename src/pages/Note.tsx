e24a8ec6710a7e15e5ece2a7907dfa9cc65c80dd
Isaiah2004 <paulisaiah@karunya.edu.in>
2024-03-20 19:58:24 +0530

    stable v1

---
+++ src/pages/Note.tsx
@@ -0,0 +1,34 @@
+import './styles.css'
+
+import { EditorContent, useEditor } from '@tiptap/react'
+import { StarterKit } from '@tiptap/starter-kit'
+import React, { useState } from 'react'
+
+import { TNote } from './types.js'
+
+const NoteEditor = ({ note }: { note: TNote }) => {
+  const [modelValue, setModelValue] = useState(note.content)
+
+  const editor = useEditor({
+    content: modelValue,
+    editorProps: {
+      attributes: {
+        class: 'm-2 p-2 border border-black rounded-lg',
+      },
+    },
+    onUpdate() {
+      setModelValue(editor?.getText() ?? '')
+    },
+    extensions: [
+      StarterKit,
+    ],
+  })
+
+  return (
+    // @ts-ignore
+    <EditorContent editor={editor}/>
+  )
+}
+NoteEditor.displayName = 'NoteEditor';
+
+export default NoteEditor;
